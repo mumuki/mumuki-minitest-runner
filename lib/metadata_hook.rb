e5bc8cd80a0e90b69f5e4120b5f69def7ac0632f
Franco Bulgarelli <franco@mumuki.org>
2017-07-06 21:20:42 -0300

    Updating metadata

--- lib/metadata_hook.rb
+++ lib/metadata_hook.rb
@@ -1,16 +1,15 @@
 class MinitestMetadataHook < Mumukit::Hook
   def metadata
     {language: {
-        name: 'ruby',
+        name: 'minitest',
         icon: {type: 'devicon', name: 'ruby'},
-        version: '2.0',
+        version: 'minitest-5.9.0/ruby-2.1',
         extension: 'rb',
         ace_mode: 'ruby'
     },
      test_framework: {
-         name: 'minitest',
-         version: '5.9.0',
-         test_extension: '.yml'
+         name: 'minitest-metatest',
+         test_extension: 'yml'
      }}
   end
 end
